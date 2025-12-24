0c83ab117a1bb160eccce83e932bb83bac09636d
Razria <dsyeom931008@gmail.com>
2025-12-24 12:36:57 +0900

    ✨add backjoon gradual practice step 01

---
+++ step_01/1008/1008/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	double a, b;
+
+	std::cin >> a >> b;
+
+	std::cout.precision(20);
+	std::cout << a / b << std::endl;
+
+	return 0;
+}
---
+++ step_01/18108/18108/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	int a;
+
+	std::cin >> a;
+
+	int b = 2541 - 1998;
+
+	std::cout << a - b << std::endl;
+
+	return 0;
+}
